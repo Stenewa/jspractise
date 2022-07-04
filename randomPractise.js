735366d146c130a7495ed466e21fda299879fce5
Stephanie Nelima <35595163+Stenewa@users.noreply.github.com>
2022-07-04 23:34:09 +0300

    Add files via upload

---
+++ randomPractise.js
@@ -0,0 +1,26 @@
+//concatinatingstrings
+//PS. As long as there is a string during concatinating, the number will be converted into a string/treated as a string
+let firstname="Abel"
+let lastname="Tesfaye"
+let greeting="Hi there,"
+let concatString=greeting+" "+firstname+" "+lastname+"!"
+//console.log(concatString);
+//function to  add and decrement points
+//fx addPoints() adds three points and removePoints removes a point
+let myPoints=3
+function addPoints(){
+    myPoints+=3
+}
+function removePoints(){
+    myPoints-=1
+}
+addPoints();
+addPoints();
+addPoints();
+removePoints();
+removePoints(); 
+//console.log(myPoints);
+
+
+
+
