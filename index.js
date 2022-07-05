3f0f8636455e3c4f6b3a9417ea4df6ebddce1a02
Stephanie Nelima <35595163+Stenewa@users.noreply.github.com>
2022-07-06 01:23:13 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -1,9 +1,20 @@
-//document.getElementById("count_el").innerText=5
+
 let count=0;
 
 function increment(){
     console.log(alert('Increase number?'))
-
     count=count+1;
+    document.getElementById("count_el").innerText=count;
+}
+function saveThis(){
+
+ 
+ let totalNumber="-"+" "+count;
+ document.getElementById("print_el").innerText+=totalNumber;
+totalNumber=new totalNumber;
+
+} 
+function reset(){
+    count=0; 
     document.getElementById("count_el").innerText=count;
 }
