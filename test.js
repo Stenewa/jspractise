6347841cd5c9e31d3a01062c2706d49d11dbe83d
Stephanie Nelima <35595163+Stenewa@users.noreply.github.com>
2023-05-19 23:10:18 +0300

    Add files via upload
    Testing the node compiler and how to execute node files on vs

---
+++ test.js
@@ -0,0 +1,9 @@
+//var thisVar = "Lorem ipsum"
+//console.log(thisVar)
+// enter the word node followed by the file name in order to run on node compiler*v8 engine*
+//use the plus sign to add vars to string
+const thisConst = (cig) => {
+    console.log('HEYYY SO CAN WE CIG AFTER S AND '+ cig);   
+}
+
+thisConst('CHILL');
